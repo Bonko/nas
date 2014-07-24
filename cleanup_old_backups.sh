339a8b17e634f15a6606131818fc58da39d521c9
Daniel Bonkowski <bonko@jimdo.com>
2014-07-24 18:49:44 +0200

    make retention time configurable

--- cleanup_old_backups.sh
+++ cleanup_old_backups.sh
@@ -2,21 +2,25 @@
 set -e
 
 BACKUPDIR="$1"
+KEEP_DAYS="$2"
 if [ -z "$BACKUPDIR" ]; then
     echo "please specify the backup directory"
 fi
+if [ -z "$KEEP_DAYS" ]; then
+    KEEP_DAYS=30
+fi
+
 # use basename in order to make backuptype better readable
 BACKUPTYPES=$(find ${BACKUPDIR} -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
-KEEP_DAYS=30
 LOGTAG=$(basename $0)
 
 cd "$BACKUPDIR"
 for dir in $BACKUPTYPES; do
-	# ensure that at least one backup is kept
-	if [ $(find "$dir" -maxdepth 1 -mindepth 1 -type d | wc -l) -lt 2 ]; then
+    # ensure that at least one backup is kept
+    if [ $(find "$dir" -maxdepth 1 -mindepth 1 -type d | wc -l) -lt 2 ]; then
         logger -t $LOGTAG "only 1 backup of type $dir found, keeping it"
         continue
-	fi
+    fi
 
     find "$dir" -maxdepth 1 -mindepth 1 -type d -mtime $KEEP_DAYS -exec logger -t $LOGTAG "removing {}" \; -exec rm -rf {} \;
 done
